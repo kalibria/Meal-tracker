3cd2299184abc327eb56d1d12f238ca77c22c4cd
Maria Kushlianskaya <mariakazakova92@gmail.com>
2022-06-07 14:41:31 +0300

    add basic welcomeComponent.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import { WelcomeComponent } from './components/welcomeComponent';
 
 function App() {
   return (
     <div>
-
+      <WelcomeComponent />
     </div>
   );
 }
