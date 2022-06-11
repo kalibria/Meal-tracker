5c19c148c8b6ab91b62c0ca801c932c5a02cf625
Maria Kushlianskaya <mariakazakova92@gmail.com>
2022-06-11 14:41:51 +0300

    add settings icon

--- src/welcome/welcomeComponent.tsx
+++ src/welcome/welcomeComponent.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
+import classes from './welcom.module.css';
 
 export function WelcomeComponent() {
   return (
-    <>
+    <div className={classes.welcome_wrapper}>
       <p>Welcome to Meal Tracker</p>
-      <button>Configure Tracker</button>
-    </>
+      <button className={classes.btn_settings}>
+        <img
+          className={classes.settingsIcon}
+          src='http://cdn.onlinewebfonts.com/svg/img_79951.png'
+          alt='settings icon'
+        />
+      </button>
+    </div>
   );
 }
