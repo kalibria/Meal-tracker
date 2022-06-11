a4a4eb351e4b370ec5acc4b5ad5b1757b7f5e004
Maria Kushlianskaya <mariakazakova92@gmail.com>
2022-06-11 13:53:04 +0300

    list of settings.MT-6

---
+++ src/settings/setings.tsx
@@ -0,0 +1,11 @@
+interface Settings {
+  timeBetweenMeals: number;
+  numberOfMealsPerDay: number;
+  numberOfMinutesToFirstMeal: number;
+}
+
+export const settingsScreen: Settings = {
+  timeBetweenMeals: 0, //from 1 to 2 hours, with a one-minute interval
+  numberOfMealsPerDay: 5, //5 is default, no max limit, 3 is min number
+  numberOfMinutesToFirstMeal: 20, //20 min is default, min 5 min, max 60 min
+};
