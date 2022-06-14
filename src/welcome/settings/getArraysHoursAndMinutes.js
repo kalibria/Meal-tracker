e5b81d2de53e26046cb13a84176ec088cf0e5143
Maria Kushlianskaya <mariakazakova92@gmail.com>
2022-06-14 14:53:31 +0300

    add zero to minutes number

--- src/welcome/settings/getArraysHoursAndMinutes.js
+++ src/welcome/settings/getArraysHoursAndMinutes.js
@@ -1,11 +1,19 @@
-export const minutesArray = [0];
+const minutesArray = [0];
 
 const getMinutes = () => {
   for (let i = 1; i <= 60; ++i) {
+    if (i.length < 2) {
+      i = '0' + i;
+    }
     minutesArray.push(i);
   }
 };
 getMinutes();
+
+const zeroPad = (num, places) => String(num).padStart(places, '0');
+export const MinutesArrayStartFromZero = minutesArray.map((num) =>
+  zeroPad(num, 2)
+);
 
 export const hoursArray = [1];
 
--- src/welcome/settings/settings_constant.tsx
+++ src/welcome/settings/settings_constant.tsx
@@ -1,6 +1,7 @@
-import { hoursArray, minutesArray } from './getArraysHoursAndMinutes';
-import { useContext } from 'react';
-import { MyContext } from '../../context/context';
+import {
+  hoursArray,
+  MinutesArrayStartFromZero,
+} from './getArraysHoursAndMinutes';
 import * as React from 'react';
 
 export interface Settings {
@@ -8,7 +9,7 @@ export interface Settings {
   numberOfMealsPerDay: { id: number; name: number };
   numberOfMinutesToFirstMeal: { id: number; name: number };
   hours: Array<number>;
-  minutes: Array<number>;
+  minutes: Array<string>;
 }
 
 export const settingsScreen: Settings = {
@@ -16,15 +17,8 @@ export const settingsScreen: Settings = {
   numberOfMealsPerDay: { id: 2, name: 5 }, //5 is default, no max limit, 3 is min number
   numberOfMinutesToFirstMeal: { id: 3, name: 20 }, //20 min is default, min 5 min, max 60 min
   hours: hoursArray,
-  minutes: minutesArray,
+  minutes: MinutesArrayStartFromZero,
 };
 
 // export const jsonSettingsScreen = JSON.stringify(settingsScreen);
 // export const jsonSettingsList = JSON.parse(jsonSettingsScreen);
-
-//
-// const optionMinute = minutes.map((min) => (
-//     <option key={min} value={min}>
-//       {min}
-//     </option>
-// ));
