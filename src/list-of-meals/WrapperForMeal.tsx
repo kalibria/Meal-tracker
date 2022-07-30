dc066e55117ee6105b65f370e5ffab641762e074
Maria Kazakova <mariakazakova92@gmail.com>
2022-07-30 15:26:16 +0300

    add mealList on screen

--- src/list-of-meals/WrapperForMeal.tsx
+++ src/list-of-meals/WrapperForMeal.tsx
@@ -1,18 +1,18 @@
 import React, { useEffect, useState } from 'react';
 import { mealsManagerBL } from './Meals';
 import { mealMapper } from './meal.mapper';
+import { Meal } from './Meal';
 
 export const WrapperForMeal = () => {
-  // const [allMeals, setAllMeals] = useState(() => {
-  //   return mealMapper.fromBLToUi(mealsManagerBL.getMealListBL());
-  // });
-  useEffect(() => {
-    const mealListBl = mealsManagerBL.getMealListBL();
-    const mealListUI = mealMapper.fromBLToUi(mealListBl);
+  const [allMeals, setAllMeals] = useState(() => {
+    return mealMapper.fromBLToUi(mealsManagerBL.getMealListBL());
+  });
 
-    console.log('mealListBl', mealListBl);
-    console.log('mealListUI', mealListUI);
-  }, []);
+  const mealsForUi = allMeals.map((item) => {
+    return (
+      <Meal key={item.number} number={item.number} timeOfMeal={item.mealTime} />
+    );
+  });
 
-  return <div>hhgfdg</div>;
+  return <div>{mealsForUi} </div>;
 };
