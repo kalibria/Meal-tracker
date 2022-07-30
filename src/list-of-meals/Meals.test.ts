c3b25e4ba1638656cc6cd648cb990f6c760f108b
Maria Kazakova <mariakazakova92@gmail.com>
2022-07-30 14:27:09 +0300

    write tests on getMealListBl

--- src/list-of-meals/Meals.test.ts
+++ src/list-of-meals/Meals.test.ts
@@ -1,4 +1,4 @@
-import { MealsManagerBL, mealsManagerBL } from './Meals';
+import { MealsManagerBL } from './Meals';
 import { LocalStorage } from '../utility/LocalStorage';
 
 describe('MealsManager', () => {
@@ -11,6 +11,7 @@ describe('MealsManager', () => {
           numberOfMinutesToFirstMeal: { id: 3, time: '20' },
         });
       }
+
       return null;
     }),
   };
@@ -19,8 +20,16 @@ describe('MealsManager', () => {
     value: localStorageMock,
   });
 
-  describe('accumulateAllMealsTimes', () => {
+  describe('Meal list', () => {
     beforeEach(() => {
+      jest.spyOn(localStorage, 'getItem').mockReturnValue(
+        JSON.stringify({
+          timeBetweenMeals: { id: 1, time: 120 },
+          numberOfMealsPerDay: { id: 2, time: '6' },
+          numberOfMinutesToFirstMeal: { id: 3, time: '20' },
+        })
+      );
+
       jest.useFakeTimers();
       jest.setSystemTime(new Date('2022-07-29 09:00'));
     });
@@ -29,32 +38,37 @@ describe('MealsManager', () => {
       jest.useRealTimers();
     });
 
-    it('should output correct times of first three meals per day', () => {
-      jest.spyOn(global.localStorage, 'getItem').mockReturnValue(
-        JSON.stringify({
-          timeBetweenMeals: { id: 1, time: 120 },
-          numberOfMealsPerDay: { id: 2, time: '6' },
-          numberOfMinutesToFirstMeal: { id: 3, time: '20' },
-        })
-      );
-
+    it('should generate first meal at current time plus time before first meal (default is 20 min)', () => {
       const mockLS = new LocalStorage(false);
       const mockMealManager = new MealsManagerBL(mockLS);
 
-      const [firstMealTime, secondMealTime, thirdMealTime] =
-        mockMealManager.accumulateAllMealsTimes();
+      const [firstMeal] = mockMealManager.getMealListBL();
 
-      expect(new Date(firstMealTime).toLocaleTimeString('ru-RU')).toContain(
-        '09:00'
-      );
+      expect(
+        new Date(firstMeal.mealTime).toLocaleTimeString('ru-RU')
+      ).toContain('09:20');
+    });
 
-      expect(new Date(secondMealTime).toLocaleTimeString('ru-RU')).toContain(
-        '11:00'
-      );
+    it('should place second meal 2h after first meal', () => {
+      const mockLS = new LocalStorage(false);
+      const mockMealManager = new MealsManagerBL(mockLS);
 
-      expect(new Date(thirdMealTime).toLocaleTimeString('ru-RU')).toContain(
-        '13:00'
-      );
+      const [, secondMeal] = mockMealManager.getMealListBL();
+
+      expect(
+        new Date(secondMeal.mealTime).toLocaleTimeString('ru-RU')
+      ).toContain('11:20');
+    });
+
+    test('first meal should have order number 1', () => {
+      const mockLS = new LocalStorage(false);
+      const mockMealManager = new MealsManagerBL(mockLS);
+
+      const [firstMeal, ...otherMeals] = mockMealManager.getMealListBL();
+      const lastMeal = otherMeals.at(-1);
+
+      expect(firstMeal.number).toBe(1);
+      expect(lastMeal?.number).toBe(6);
     });
   });
 });
