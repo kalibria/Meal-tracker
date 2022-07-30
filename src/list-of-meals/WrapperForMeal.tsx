a6840d26eb6e300154c8f1c7c7566f0a81e22a24
Maria Kazakova <mariakazakova92@gmail.com>
2022-07-30 15:17:32 +0300

    add MealMapper with method FromBLToUI that return meals for ui

--- src/list-of-meals/WrapperForMeal.tsx
+++ src/list-of-meals/WrapperForMeal.tsx
@@ -1,12 +1,18 @@
 import React, { useEffect, useState } from 'react';
 import { mealsManagerBL } from './Meals';
+import { mealMapper } from './meal.mapper';
 
 export const WrapperForMeal = () => {
-  const [allMeals, setAllMeals] = useState(() => {
-    return mealsManagerBL.getMealListBL();
-  });
+  // const [allMeals, setAllMeals] = useState(() => {
+  //   return mealMapper.fromBLToUi(mealsManagerBL.getMealListBL());
+  // });
+  useEffect(() => {
+    const mealListBl = mealsManagerBL.getMealListBL();
+    const mealListUI = mealMapper.fromBLToUi(mealListBl);
 
-  console.log('allTimes', allMeals);
+    console.log('mealListBl', mealListBl);
+    console.log('mealListUI', mealListUI);
+  }, []);
 
   return <div>hhgfdg</div>;
 };
---
+++ src/list-of-meals/meal.mapper.ts
@@ -0,0 +1,25 @@
+import { format } from 'date-fns';
+import { IMealBL } from './Meals';
+
+interface IMealItemUi {
+  number: number;
+  mealTime: string;
+  eaten: boolean;
+  edit: boolean;
+  delete: boolean;
+}
+
+class MealMapper {
+  fromBLToUi(mealList: Array<IMealBL>): Array<IMealItemUi> {
+    return mealList.reduce((acc: IMealItemUi[], mealItem) => {
+      const timeForUI = format(mealItem.mealTime, "HH ':' mm");
+
+      const itemForUI = { ...mealItem, mealTime: timeForUI };
+      acc.push(itemForUI);
+
+      return acc;
+    }, []);
+  }
+}
+
+export const mealMapper = new MealMapper();
