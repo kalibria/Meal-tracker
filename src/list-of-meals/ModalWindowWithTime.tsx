8fb6e09e3233783624b50a542dc088a67f0b7642
Maria Kazakova <mariakazakova92@gmail.com>
2022-08-12 14:31:23 +0300

    add minutes in ModalWindowWithTime

--- src/list-of-meals/ModalWindowWithTime.tsx
+++ src/list-of-meals/ModalWindowWithTime.tsx
@@ -3,19 +3,25 @@ import InputLabel from '@mui/material/InputLabel';
 import MenuItem from '@mui/material/MenuItem';
 import FormControl from '@mui/material/FormControl';
 import Select, { SelectChangeEvent } from '@mui/material/Select';
-import { time } from '../settings/time';
-import { timeManager } from '../utility/time.manager';
+import { minutes, time } from '../settings/time';
 import { currentTime } from '../utility/currentTime';
 
 export default function ModalWindowWithTime() {
-  const [hour, setHour] = React.useState(
+  const [currentHour, setCurrentHour] = React.useState(
     currentTime.getCurrentHours().toString()
+  );
+  const [currentMinutes, setCurrentMinutes] = React.useState(
+    currentTime.getCurrentMinutes().toString()
   );
 
   const listOfHours = time.convertMinutes(time.prepareHoursForModalWindow());
+  const listOfMinutes = minutes;
 
-  const handleChange = (event: SelectChangeEvent) => {
-    setHour(event.target.value);
+  const handleHoursChange = (event: SelectChangeEvent) => {
+    setCurrentHour(event.target.value);
+  };
+  const handleMinutesChange = (event: SelectChangeEvent) => {
+    setCurrentMinutes(event.target.value);
   };
 
   const menuHourItems = listOfHours.map((item) => {
@@ -25,22 +31,47 @@ export default function ModalWindowWithTime() {
       </MenuItem>
     );
   });
+  const menuMinutesItems = listOfMinutes.map((item) => {
+    return (
+      <MenuItem key={item} value={item}>
+        {item}
+      </MenuItem>
+    );
+  });
 
   return (
-    <FormControl sx={{ m: 1, minWidth: 120 }} size='small'>
-      <InputLabel id='demo-select-small'>Hours</InputLabel>
-      <Select
-        labelId='demo-select-small'
-        id='demo-select-small'
-        value={hour}
-        label='Hours'
-        onChange={handleChange}
-      >
-        <MenuItem value={hour}>
-          <em>{hour}</em>
-        </MenuItem>
-        {menuHourItems}
-      </Select>
-    </FormControl>
+    <div>
+      <FormControl sx={{ m: 1, minWidth: 120 }} size='small'>
+        <InputLabel id='hours-select-small'>Hours</InputLabel>
+        <Select
+          labelId='hours-select-small'
+          id='hours-select-small'
+          value={currentHour}
+          label='Hours'
+          onChange={handleHoursChange}
+        >
+          <MenuItem value={currentHour}>
+            <em>{currentHour}</em>
+          </MenuItem>
+          {menuHourItems}
+        </Select>
+      </FormControl>
+
+      <FormControl sx={{ m: 1, minWidth: 120 }} size='small'>
+        <InputLabel id='minutes-select-small'>Minutes</InputLabel>
+        <Select
+          labelId='minutes-select-small'
+          id='minutes-select-small'
+          value={currentMinutes}
+          label='Minutes'
+          onChange={handleMinutesChange}
+        >
+          <MenuItem value={currentMinutes}>
+            <em>{currentMinutes}</em>
+          </MenuItem>
+          {menuMinutesItems}
+        </Select>
+      </FormControl>
+    </div>
   );
 }
