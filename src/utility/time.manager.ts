8a6e4ddabca7b1c003aee6fbc4600a042a08b738
Maria Kazakova <mariakazakova92@gmail.com>
2022-09-14 12:53:16 +0300

    remove unused dependences

--- src/utility/time.manager.ts
+++ src/utility/time.manager.ts
@@ -1,11 +1,5 @@
 import { format, parse } from 'date-fns';
 import { IMealBL } from '../list-of-meals/mealsManager';
-import {
-  defaultLatestTime,
-  timeOfLastMeal,
-} from '../list-of-meals/constantOfListOfMeal';
-import { myLocalStorage } from './LocalStorage';
-import { minutesInHour } from '../settings/settings_constant';
 
 class TimeManager {
   timeFromBLToUI(timeMs: number) {
@@ -28,25 +22,6 @@ class TimeManager {
     const minutesUi = timeUi.slice(5, 7);
     return minutesUi;
   }
-
-  // latestMealTime = (time: number) => {
-  //   // const timeOfLastMealTimeBL = this.timeFromUIToBL(timeOfLastMeal);
-  //   const defaultLatestMealTimeBL = this.timeFromUIToBL(defaultLatestTime);
-  //   const timeBetweenMealsMS =
-  //     myLocalStorage.getTimeBetweenMeals() * minutesInHour * 1000;
-  //
-  //   const latestMealTime = time +
-  //
-  //   if (time > timeOfLastMealTimeBL) {
-  //     if (time + timeBetweenMealsMS < defaultLatestMealTimeBL) {
-  //       return time + timeBetweenMealsMS;
-  //     } else {
-  //       return 0;
-  //     }
-  //   } else {
-  //     return time + timeBetweenMealsMS;
-  //   }
-  // };
 }
 
 export const timeManager = new TimeManager();
